85cae55a229614fee18c89d1e2f371e5c04a4125
gornje <zlatko.gornjevski@gmail.com>
2019-08-17 01:24:29 +0200

    Homework 1

---
+++ WebApp1/Controllers/UsersController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace WebApp1.Controllers
+{
+
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        public static List<User> users = new List<User>()
+        {
+            new User(){ FirstName = "John", LastName = "Doe", Age = 22},
+            new User(){ FirstName = "Jane", LastName = "Deal", Age = 16},
+            new User(){ FirstName = "Shawn", LastName = "Pawn", Age = 44},
+            new User(){ FirstName = "Mike", LastName = "Trix", Age = 17}
+        };
+        [HttpGet]
+        public ActionResult<List<User>> Get()
+        {
+            return users;
+        }
+        [HttpGet("{id}")]
+        public ActionResult<User> Get(int id)
+        {
+            try
+            {
+                return users[id - 1];
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                return NotFound($"User with id - {id}, does not exist! Try again!");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Something went wrong: {ex.Message}");
+            }
+        }
+        [HttpGet("age/{id}")]
+        public ActionResult<User> CheckAdult(int id)
+        {
+            try
+            {
+                if(users[id-1].Age >= 18)
+                {
+                    return Ok($"The user with name {users[id - 1].FirstName} {users[id - 1].LastName} and id {id} is adult!");
+                }
+                else
+                {
+                    return Ok($"The user with name {users[id - 1].FirstName} {users[id - 1].LastName} and id {id} is not an adult!");
+                }
+
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                return NotFound($"User with id - {id}, does not exist! Try again!");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Something went wrong: {ex.Message}. Try again!");
+            }
+        }
+        [HttpPost]
+        public IActionResult Post()
+        {
+            string body;
+            using (StreamReader sr = new StreamReader(Request.Body))
+            {
+                body = sr.ReadToEnd();
+            }
+            User user = JsonConvert.DeserializeObject<User>(body);
+            users.Add(user);
+            return Ok($"User with id {users.Count} has been added to the list!");
+        }
+    }
+}
---
+++ WebApp1/wwwroot/users_script.js
@@ -0,0 +1,56 @@
+﻿let allUsersBtn = document.getElementById("button1");
+let userByIdBtn = document.getElementById("button2");
+let checkIfUserAdultBtn = document.getElementById("button3");
+let addNewUserBtn = document.getElementById("button4");
+let userByIdInput = document.getElementById("inputField2");
+let checkAdultInput = document.getElementById("inputField3");
+let newUserInput1 = document.getElementById("inputField4fn");
+let newUserInput2 = document.getElementById("inputField4ln");
+let newUserInput3 = document.getElementById("inputField4Ag");
+
+let port = "58554"
+
+let allUsers = async () => {
+    let url = "http://localhost:" + port + "/api/Users";
+    console.log(url);
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+};
+
+let userById = async () => {
+    let url = "http://localhost:" + port + "/api/users/" + userByIdInput.value;
+
+    let response = await fetch(url);
+    let data = await response.text();
+    console.log(data);
+};
+
+let checkIfUserAdult = async () => {
+    let url = "http://localhost:" + port + "/api/users/age/" + checkAdultInput.value;
+
+    let response = await fetch(url);
+    try {
+        let data = await response.text();
+        console.log(data);
+    } catch (e) {
+        console.log("There was a problem with this request");
+    }
+};
+
+let newUser = async () => {
+    let url = "http://localhost:" + port + "/api/users";
+    let user = { FirstName: newUserInput1.value, LastName: newUserInput2.value, Age: newUserInput3.value };
+    await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(user)
+    });
+}
+
+allUsersBtn.addEventListener("click", allUsers);
+userByIdBtn.addEventListener("click", userById);
+checkIfUserAdultBtn.addEventListener("click", checkIfUserAdult);
+addNewUserBtn.addEventListener("click", newUser);
